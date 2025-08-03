29e096dbff8e586814653ddfbc8504015323b7db
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-03 08:36:23 +0000

    feature: 기본 폰트 Noto Sans KR 로 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 'use client';
 
 import GlobalStyle from '~/style/GlobalStyle';
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKR = Noto_Sans_KR({
+  weight: '400',
+});
 
 export default function RootLayout({
   children,
@@ -10,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <GlobalStyle />
-      <body>{children}</body>
+      <body className={notoSansKR.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
 export default function Home() {
-  return <main></main>;
+  return <main>Noto Sans KR 적용 확인</main>;
 }
